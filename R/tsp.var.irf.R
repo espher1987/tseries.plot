0a237ab9ff4094bbf2d9bd5c73187466fc65160d
espher1987 <espher1987@hotmail.com>
2018-11-17 11:43:52 -0600

    irf VAR plot

---
+++ R/tsp.var.irf.R
@@ -0,0 +1,68 @@
+#' Irf VAR plot
+#'
+#' @param irf irf class object created by vars::irf() function
+#'
+#' @return A ggplot2 object class
+#' @export
+#' @importFrom dplyr as_tibble tally mutate bind_rows
+#' @importFrom tidyr gather
+#' @importFrom ggplot2 ggplot geom_ribbon aes geom_line geom_hline facet_grid vars scale_x_continuous
+#'
+
+tsp.var.irf <- function(irf){
+  nlag <- irf$irf[[1]] %>% as_tibble() %>% tally() %>% as.numeric() -1
+  nimp <- length(irf$impulse)
+  nres <- length(irf$response)
+
+  df.mean <- list()
+  df.lower <- list()
+  df.upper <- list()
+
+  for (i in 1:nimp) {
+    df.mean[[i]]  <-   irf$irf[[i]] %>%
+      as_tibble() %>%
+      mutate(impulse = paste("impulse:",names(irf$Upper[i])),
+             lag = 0:nlag) %>%
+      gather("response",
+             value = "value",
+             -impulse,
+             -lag)
+  }
+
+  for (i in 1:nimp) {
+    df.lower[[i]]  <-   irf$Lower[[i]] %>%
+      as_tibble() %>%
+      mutate(impulse = paste("impulse:",names(irf$Upper[i])),
+             lag = 0:nlag) %>%
+      gather("response",
+             value = "value",
+             -impulse,
+             -lag)
+  }
+
+  for (i in 1:nimp) {
+    df.upper[[i]]  <-   irf$Upper[[i]] %>%
+      as_tibble() %>%
+      mutate(impulse = paste("impulse:",names(irf$Upper[i])),
+             lag = 0:nlag) %>%
+      gather("response",
+             value = "value",
+             -impulse,
+             -lag)
+  }
+
+  df.mean  <- bind_rows(df.mean)
+  df.lower <- bind_rows(df.lower)
+  df.upper <- bind_rows(df.upper)
+
+  ggplot(df.mean) +
+    geom_ribbon(mapping = aes(x     = df.mean$lag,
+                              ymin  = df.lower$value,
+                              ymax = df.upper$value),
+                alpha = 0.2) +
+    geom_line(data = df.mean,mapping = aes(lag,value)) +
+    geom_hline(yintercept = 0, color = "red", lty = 2) +
+    labs(x = "lag", y = "response") +
+    facet_grid(rows = vars(response),cols = vars(impulse)) +
+    scale_x_continuous(breaks = 0:nlag)}
+
