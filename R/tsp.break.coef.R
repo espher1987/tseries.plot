a077d394ad6b728fa09c14cf6fb33ce52b4a23ee
espher1987 <espher1987@hotmail.com>
2022-10-30 22:23:11 -0600

    fix tsp.breaks.coef

--- R/tsp.break.coef.R
+++ R/tsp.break.coef.R
@@ -79,34 +79,45 @@ tsp.break.coef <- function(data,
 
   if(plot == TRUE){
     seg <- purrr::map_dfc(bk,breakfactor) %>%
-      dplyr::mutate(time = as.Date.ts(fitted(bk[[1]]))) %>%
+      dplyr::mutate(time = zoo::as.Date.ts(fitted(bk[[1]]))) %>%
       tidyr::pivot_longer(-time) %>%
       dplyr::rename(segment = value)
 
     fitted <- purrr::map_dfc(bk,fitted.values) %>%
-      dplyr::mutate(time = as.Date.ts(fitted(bk[[1]])),
-             type = "fitted") %>%
-      tidyr::pivot_longer(-c(time,type))
+      dplyr::mutate(time = zoo::as.Date.ts(fitted(bk[[1]]))) %>%
+      tidyr::pivot_longer(-c(time)) %>%
+      dplyr::rename(value_fitted = value)
 
     observed <- purrr::map_dfc(bk,function(data){data$y}) %>%
-      dplyr::mutate(time = as.Date.ts(fitted(bk[[1]])),
-             type = "observed") %>%
-      tidyr::pivot_longer(-c(time,type))
-    suppressMessages(
-    plot_data <- dplyr::full_join(fitted,seg))
+      dplyr::mutate(time = zoo::as.Date.ts(fitted(bk[[1]]))) %>%
+      tidyr::pivot_longer(-c(time)) %>%
+      dplyr::rename(value_observed = value)
 
-    plot <- ggplot2::ggplot()+
-      ggplot2::geom_line(data = observed,
-                         ggplot2::aes(time,value,
-                    lty = "observed"))+
-      ggplot2::geom_line(data = plot_data,
-                         ggplot2::aes(time,value,
-                    group = segment,
-                    lty = "fitted")) +
+    suppressMessages(
+      plot_data <- dplyr::full_join(fitted,seg) %>%
+        group_by(name,segment) %>%
+        mutate(i = 1:length(segment),
+               b = ifelse(i == max(i),as.character(time),NA)) %>%
+        full_join(observed))
+
+    pl <- ggplot2::ggplot(plot_data)+
+      ggplot2::geom_line(ggplot2::aes(time,
+                                      value_observed,
+                                      lty = "observed"))+
+      ggplot2::geom_line(ggplot2::aes(time,
+                                      value_fitted,
+                                      group = segment,
+                                      lty = "fitted")) +
+      ggplot2::geom_segment(ggplot2::aes(x = as.Date(b),
+                                         y = -Inf,
+                                         xend = as.Date(b),
+                                         yend = Inf),
+                            lty = 3) +
       ggplot2::scale_linetype_manual(values = c(fitted = 2,
-                                       observed = 1))+
-      ggplot2::facet_grid(vars(fitted$name),scales = "free_y")
-    print(plot)
+                                                observed = 1)) +
+      ggplot2::facet_grid(ggplot2::vars(name),
+                          scales = "free_y")
+  suppressWarnings(print(pl))
   }
   return(result)
 }
